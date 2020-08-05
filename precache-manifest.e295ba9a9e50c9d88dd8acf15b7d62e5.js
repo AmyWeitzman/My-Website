e621b0f595dd48d14b5b90927e955bd30cdb6e0c
AmyWeitzman <amyweitzman@yahoo.com>
2020-08-05 15:07:42 -0700

    Updates

--- precache-manifest.e295ba9a9e50c9d88dd8acf15b7d62e5.js
+++ precache-manifest.e295ba9a9e50c9d88dd8acf15b7d62e5.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "322cb0ab2d0ed90505114bd6eb3fa758",
+    "revision": "4eadc38be1ab735f777f8244ec62d0d5",
     "url": "/my-website/index.html"
   },
   {
-    "revision": "e9169202bbec41248221",
+    "revision": "899312a4405e97c442ee",
     "url": "/my-website/static/css/2.fb346933.chunk.css"
   },
   {
-    "revision": "3324c21f5bf768cfacf6",
-    "url": "/my-website/static/css/main.322b0eb6.chunk.css"
+    "revision": "e8d4ce9345d617e7d138",
+    "url": "/my-website/static/css/main.641dac36.chunk.css"
   },
   {
-    "revision": "e9169202bbec41248221",
-    "url": "/my-website/static/js/2.0ad02cd9.chunk.js"
+    "revision": "899312a4405e97c442ee",
+    "url": "/my-website/static/js/2.f2dde122.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/my-website/static/js/2.0ad02cd9.chunk.js.LICENSE.txt"
+    "url": "/my-website/static/js/2.f2dde122.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3324c21f5bf768cfacf6",
-    "url": "/my-website/static/js/main.90d427f1.chunk.js"
+    "revision": "e8d4ce9345d617e7d138",
+    "url": "/my-website/static/js/main.d801a13c.chunk.js"
   },
   {
     "revision": "dc2e3b3511453d8cdedf",
@@ -76,6 +76,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-website/static/media/chinese-food.15e33068.png"
   },
   {
+    "revision": "9a0280f804819e2ef529e39df8c7010d",
+    "url": "/my-website/static/media/email.9a0280f8.png"
+  },
+  {
     "revision": "570f071410c71622e10af892a73be2c7",
     "url": "/my-website/static/media/football.570f0714.png"
   },
@@ -100,8 +104,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-website/static/media/interviewq.980ac494.jpg"
   },
   {
+    "revision": "28c4a8474dc0d5bab0a937a1f0cd5072",
+    "url": "/my-website/static/media/linkedin.28c4a847.png"
+  },
+  {
     "revision": "2c57d446466ae1a8061ecf2ad44bd280",
     "url": "/my-website/static/media/niners-logo.2c57d446.png"
+  },
+  {
+    "revision": "ef468e275a2c13d0bc7156549261fde6",
+    "url": "/my-website/static/media/octocat.ef468e27.png"
   },
   {
     "revision": "5ac886394c94a6362ecc723e9c1f675a",
@@ -138,6 +150,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "40602e5dc11b23ae8225050d123d334e",
     "url": "/my-website/static/media/reeses.40602e5d.png"
+  },
+  {
+    "revision": "c903d4ce9f89f25eaf51f45617680672",
+    "url": "/my-website/static/media/resume.c903d4ce.png"
   },
   {
     "revision": "18b4885b08bc48080a7d4972512f93d9",
