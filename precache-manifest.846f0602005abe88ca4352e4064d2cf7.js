b17d4e39e49305e0251a68655c634faf23d83670
AmyWeitzman <amyweitzman@yahoo.com>
2020-08-05 13:57:44 -0700

    Updates

---
+++ precache-manifest.846f0602005abe88ca4352e4064d2cf7.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322cb0ab2d0ed90505114bd6eb3fa758",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "e9169202bbec41248221",
+    "url": "/my-website/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "3324c21f5bf768cfacf6",
+    "url": "/my-website/static/css/main.322b0eb6.chunk.css"
+  },
+  {
+    "revision": "e9169202bbec41248221",
+    "url": "/my-website/static/js/2.0ad02cd9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-website/static/js/2.0ad02cd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3324c21f5bf768cfacf6",
+    "url": "/my-website/static/js/main.90d427f1.chunk.js"
+  },
+  {
+    "revision": "dc2e3b3511453d8cdedf",
+    "url": "/my-website/static/js/runtime-main.dac0fb3d.js"
+  },
+  {
+    "revision": "dcdf655f4ed5ddbbb3d0f0e7d8648d14",
+    "url": "/my-website/static/media/BANs1.dcdf655f.jpg"
+  },
+  {
+    "revision": "eb1937cf62875c5c72ae2ce78c237369",
+    "url": "/my-website/static/media/BANs2.eb1937cf.PNG"
+  },
+  {
+    "revision": "3516b43b42e8e0e7a9c53d5a3f3c23e0",
+    "url": "/my-website/static/media/BANs3.3516b43b.PNG"
+  },
+  {
+    "revision": "fd0e53ec4085ef60233c112036fbfbc1",
+    "url": "/my-website/static/media/BANs4.fd0e53ec.PNG"
+  },
+  {
+    "revision": "b94f09e3c1f84b2be18aae5b0a852ea9",
+    "url": "/my-website/static/media/BANs5.b94f09e3.PNG"
+  },
+  {
+    "revision": "eef62fd8314e4e99004d17d5d3efde01",
+    "url": "/my-website/static/media/BANs6.eef62fd8.PNG"
+  },
+  {
+    "revision": "a41383d6b6eb9f835515a2e3d94c606b",
+    "url": "/my-website/static/media/BANs7.a41383d6.jpg"
+  },
+  {
+    "revision": "5d29129f76204b1b13eb78afcc20a941",
+    "url": "/my-website/static/media/VR_edu.5d29129f.jpg"
+  },
+  {
+    "revision": "b2b20f8841b7174a95990459172f3d8c",
+    "url": "/my-website/static/media/bama_logo.b2b20f88.png"
+  },
+  {
+    "revision": "e913a043ea97580679b8d0b1c124d7d1",
+    "url": "/my-website/static/media/big-bang-theory.e913a043.jpg"
+  },
+  {
+    "revision": "f39f569c3cd66ffdbafacd89df0ce3b6",
+    "url": "/my-website/static/media/brownie.f39f569c.png"
+  },
+  {
+    "revision": "15e3306879734ff4d4fd7daa7728ea7b",
+    "url": "/my-website/static/media/chinese-food.15e33068.png"
+  },
+  {
+    "revision": "570f071410c71622e10af892a73be2c7",
+    "url": "/my-website/static/media/football.570f0714.png"
+  },
+  {
+    "revision": "8a39d5d17696fa5c2f36ebc37fb02cb3",
+    "url": "/my-website/static/media/friends.8a39d5d1.png"
+  },
+  {
+    "revision": "c2eda8a94321bf59feb647a420b97ea7",
+    "url": "/my-website/static/media/gray.c2eda8a9.png"
+  },
+  {
+    "revision": "d149675c66209c31496c16fecc302955",
+    "url": "/my-website/static/media/himym.d149675c.png"
+  },
+  {
+    "revision": "352c37ab2e154373b64db0339101e56d",
+    "url": "/my-website/static/media/hobby-cloud.352c37ab.PNG"
+  },
+  {
+    "revision": "980ac494087ad0d87404b4c9e0b83d24",
+    "url": "/my-website/static/media/interviewq.980ac494.jpg"
+  },
+  {
+    "revision": "2c57d446466ae1a8061ecf2ad44bd280",
+    "url": "/my-website/static/media/niners-logo.2c57d446.png"
+  },
+  {
+    "revision": "5ac886394c94a6362ecc723e9c1f675a",
+    "url": "/my-website/static/media/olive.5ac88639.png"
+  },
+  {
+    "revision": "c36136eba2a8949957371fda11138381",
+    "url": "/my-website/static/media/oreo.c36136eb.png"
+  },
+  {
+    "revision": "f28b67b91df2b4fce0ec8ead7af9eaff",
+    "url": "/my-website/static/media/pancakes.f28b67b9.png"
+  },
+  {
+    "revision": "418f6bd587563fe9eb605458f64263d6",
+    "url": "/my-website/static/media/personal_edu.418f6bd5.png"
+  },
+  {
+    "revision": "f6265e08fb65cf48fd255298fd6f356e",
+    "url": "/my-website/static/media/pic_of_me.f6265e08.jpg"
+  },
+  {
+    "revision": "73fc1a219467c13f678476345dba977f",
+    "url": "/my-website/static/media/pizza.73fc1a21.png"
+  },
+  {
+    "revision": "9591ed82caa8d20c30db96cb7298d3a9",
+    "url": "/my-website/static/media/python.9591ed82.png"
+  },
+  {
+    "revision": "8f65a03390d620df8a81faebb72bffb4",
+    "url": "/my-website/static/media/react.8f65a033.png"
+  },
+  {
+    "revision": "40602e5dc11b23ae8225050d123d334e",
+    "url": "/my-website/static/media/reeses.40602e5d.png"
+  },
+  {
+    "revision": "18b4885b08bc48080a7d4972512f93d9",
+    "url": "/my-website/static/media/ripple2.18b4885b.jpg"
+  },
+  {
+    "revision": "140d84d1f934e6280941ce5f4325ab5e",
+    "url": "/my-website/static/media/seinfeld.140d84d1.png"
+  },
+  {
+    "revision": "813f5d0018026bad79e8a194f2bc3f9b",
+    "url": "/my-website/static/media/snickers.813f5d00.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61b76ed09771699e400f4d773d3f7618.js"
+  "/my-website/precache-manifest.846f0602005abe88ca4352e4064d2cf7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
